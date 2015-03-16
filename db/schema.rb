2a9c795f1805db1d67b55c00024621d0c67a95c3
andrey1983 <andrey.gu.c@gmail.com>
2015-03-17 00:36:29 +0400

    Изменил модель User

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315112134) do
+ActiveRecord::Schema.define(version: 20150316203137) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.integer  "access"
+    t.float    "date_last_visited"
+    t.integer  "bann"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
 end
