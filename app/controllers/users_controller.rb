28ed98f61f02ba27ce28e20fd0622731d21f0e86
andrey1983 <andrey.gu.c@gmail.com>
2015-03-17 16:12:51 +0400

    edit form Users and create view show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.new
 		@var_time = Time.now.to_time.to_i
 	end
+
+	def show
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true, uniqueless: true
+	validates :name, presence: true, uniqueness: true
   has_secure_password
 end
