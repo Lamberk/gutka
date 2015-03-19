919b790eed112bfa37cfef0f7f6728eaf70f1537
andrey1983 <andrey.gu.c@gmail.com>
2015-03-20 00:49:09 +0400

    create form for add/edit users and create show all users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(name: params[:name])
+  	if user and user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		redirect_to users_url
+  	else
+  		render plain: 'Все ОК!'
+  	end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,44 @@
 class UsersController < ApplicationController
 	def index
 		@var_date_now = Time.now.to_time.to_i
+		@users = User.order(:date_last_visited, ":desc")
 	end
 
 	def new
-		
+		@user = User.new
+		@caption = 'Новый пользователь'
 	end
 
-	def show
-
+	def edit
+		@caption = 'Изменение информации'
+		@user = User.find(params[:id])
 	end
 
 	def create
-		
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to users_url
+		else
+			render 'new'
+		end
 	end
 
 	def update
-		
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to users_url
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
 		
 	end
+
+	private
+	  def user_params
+	    params.require(:user).permit(:name, :username, :password)
+	  end
 
 end
