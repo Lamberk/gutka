a4e40a91275a17e556b37db2c0445ad11e71f5ff
andrey1983 <andrey.gu.c@gmail.com>
2015-04-07 12:38:39 +0400

    Обновил процесс авторизации (теперь при входе в таблицу с пользователями добавляется время входа)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,12 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(username: params[:username])
+  	time = Time.now.to_i.to_s
+  	query = ActiveRecord::Base.connection()
   	if user and user.authenticate(params[:password])
   		session[:user_id] = user.id
+  		user_id = user.id.to_s
+  		query.execute("UPDATE users SET date_last_visited = " +time+ " WHERE id = " +user_id)
   		redirect_to users_url
   	else
   		redirect_to login_url
